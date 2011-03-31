c82366366415ebd65d25fba893ea41549772eb45
Mark Headd <mheadd@voiceingov.org>
2011-03-30 22:02:32 -0400

    Modified example to match recent API changes.

--- example.js
+++ example.js
@@ -4,32 +4,33 @@
 var sys = require('sys');
 var Open311 = require('./lib/open311').Open311;
 
-// Options for the City of Boston (see https://mayors24.cityofboston.gov:6443/open311/v2/apps).
+// Options for the City of Boston (see https://mayors24.cityofboston.gov:6443/api/open311/v2/apps/).
 var options = {
-		'endpoint': 'mayors24.cityofboston.gov', 
-		'service_path': '/open311/v2/', 
-		'jurisdiction_id': 'cityofboston.gov', 
-		'secure': true,
-		'port': 6443
-		};
+        'endpoint': 'mayors24.cityofboston.gov',
+        'service_path': '/api/open311/v2/',
+        'jurisdiction_id': 'cityofboston.gov',
+        'secure': true,
+        'port': 6443
+        };
+
+// Variable to hold the response code from the Open311 API.
+var responseCode;
 
 var report = new Open311(options);
 report.getServiceList('json');
 
 report.addListener('responseCode', function(code) {
-	sys.puts('Response code: ' + code);
+    responseCode = code;
+    sys.puts('Response code: ' + responseCode);
 });
 
 report.addListener('responseBody', function(body) {
-	var service_list = JSON.parse(body);
-	sys.puts('Response body: ');
-	sys.puts(body);
-	sys.puts('');
-    for(var i = 0; i < service_list.services.length; i++) {
-        sys.puts('Service Name: ' + service_list.services[i].service_name);
-        sys.puts('Service Code: ' + service_list.services[i].service_code);
+  if(responseCode == '200') {
+    var service_list = JSON.parse(body);
+    for(var i = 0; i < service_list.length; i++) {
+        sys.puts('Service Name: ' + service_list[i].service_name);
+        sys.puts('Service Code: ' + service_list[i].service_code);
         sys.puts('');
     }
-
-	
+  }
 });
